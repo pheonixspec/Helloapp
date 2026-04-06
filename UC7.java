f82240c61cffd6dc44d082cbb7d76b8de44e7d8c
pheonixspec <kshitijattri@gmail.com>
2026-04-06 12:48:13 +0530

    use case 7

---
+++ UC7.java
@@ -0,0 +1,11 @@
+public class UC7 {
+    public static void main(String[] args) {
+        // Check whether arguments are provided
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            // Use String.join() to efficiently concatenate all names with a comma-space delimiter
+            System.out.println("Hello, " + String.join(", ", args) + "!");
+        }
+    }
+}
